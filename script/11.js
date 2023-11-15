ffbe4ba4baafe456dbaef8693ff2e1d6b549dbf0
hyun3936 <hyun3936@naver.com>
2023-11-15 13:07:30 +0900

    11/15

--- script/11.js
+++ script/11.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
     //회문처리
     bt1.addEventListener("click", (e)=>{
-        e.preventDefault();
+        e.preventDefault(); //preventDefault() 메소드는 이벤트가 발생했을 때 브라우저가 수행하는 기본 동작을 중단시키는 역할을 합니다.
         let result = ' ' ;
 
       
@@ -15,18 +15,32 @@ document.addEventListener("DOMContentLoaded", ()=>{
             result = result + txt1.value[i];
         }
 
-        console.log(result);
+        console.log(result);  // 개발자 도구의 콘솔에 메시지를 출력하는 데에 사용 ==> 디버그 하는 용도로 쓰기 좋음.
+
         if (result == txt1.value) txt2.value = '회문입니다.';
         else txt2.value = '회문이 아닙니다.';
 
     });
 
-
+  
 
     //숫자 합계
-    bt2.addEventListener("click", ()=>{
+    bt2.addEventListener("click", (e)=>{
+        e.preventDefault(); // 화면이 깜빡거리면서 다시 부르는걸 막음 
 
+        let sum = 0 ;  // 합계를 저장할 변수
+
+        // for(let i = 0; i < txt1.value.length; i++){
+        //     console.log(txt1.value[i]);
+        // }
+        console.log(sum) // 디버그용
+        
+        for (let c of txt1.value) {
+        if(!isNaN(c))  // 숫자 인지아닌지 판별 , 그 자체로 false 또는 true 이기때문에 굳이 if(isNaN(c) = true) 이런식으로 쓸 필요x
+            sum = sum + parseInt(c);
+        }
+
+        txt2.value = sum;
 
     });
-    
 });
---
+++ script/12.js
@@ -0,0 +1,113 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+        // 입력 버튼
+      const bt11 = document.querySelectorAll(".bt11");
+    //삭제 버튼
+      const bt12 = document.querySelectorAll(".bt12");
+
+      
+      const txt1 = document.querySelector("#txt1")
+
+        //   let arr = new Array() ; // 추천x
+        let arr = []; // 대부분 이거씀. 배열의 선언
+        // let은 변수를 선언하는 키워드입니다. 
+        // let을 사용하여 변수를 선언하면 해당 변수는 블록 스코프를 갖습니다.
+        // 즉, 변수가 선언된 블록 내에서만 유효하며 블록을 벗어나면 사용할 수 없습니다.
+
+
+
+            // switch case
+            // let emoji ; // 윈도우 이모지 단축키 win + .
+            // switch (bt.innerHTML.trim()){ // trim() 앞뒤 공백제거(중간공백은x) , 공백이 있으면 이모지 안나올 수도 있음.
+            //     case '사과' : emoji = '🍎' ; break;   // switch case문은 break 필수!
+            //     case '바나나' : emoji = '🍌' ; break; 
+            //     case '오렌지' : emoji = '🍊' ; break; 
+            //     case '수박' : emoji = '🍉' ; break;
+            // }
+
+            // 그냥 if
+            // if (bt.innerHTML == '사과') emoji = '🍎' 
+
+
+
+            // 자료구조를 잘쓰면 이렇게 간단하게 가능
+    
+            // 오브젝트(object)
+            const emoji = { '사과':'🍎',
+                            '바나나':'🍌',        
+                            '오렌지':'🍊',
+                            '수박':'🍉',
+                            '당근':'🥕',
+                            '오이':'🥒',
+                            '아보카도':'🥑',
+                            '브로콜리':'🥦',                        
+                          }
+
+                          //입력 처리
+                          
+
+            for(let bt of bt11){
+                bt.addEventListener("click", (e)=>{
+                    e.preventDefault();
+
+            // 조건식으로 처리하지 않고 오브젝트로 처리
+            arr.push(emoji[bt.innerHTML.trim()]); // 배열에 추가 
+         
+            txt1.value = arr.join(" ");
+        });
+    }
+
+
+
+    // 삭제처리
+    // ★★★★★★★  map & filter ★★★★★★★★ 엄청 중요 (자주 씀) - 배열을 돎. (but, filter는 조건에 맞는것만 골라냄.)
+    // 둘다 콜백함수를 가짐. 함수는 'return'을 가진다.
+    // arr = arr.filter (()=>{
+    //    return ㅇㅇㅇ // arr변수가 return값을 가진다.
+    // })
+
+    for(let bt of bt12) {
+        bt.addEventListener("click", (e)=>{
+            e.preventDefault();
+
+           // arr = arr.map((item, idx) => {
+           //     let key = bt.innerHTML.slice(0, -2);
+               
+            //    return item != emoji[key];
+            //}) ;
+
+            // 식 단축
+            arr = arr.filter(item => item != emoji[bt.innerHTML.slice(0, -2)]);
+
+            txt1.value = arr.join(' ');
+        })
+    }
+
+
+
+
+    //변경처리
+    for(let bt of bt13){
+        bt.addEventListener("click", (e)=>{
+            e.preventDefault();
+            
+
+            let key = bt.innerHTML;
+            key = key.split('+')
+   //         console.log(emoji[key[0]], emoji[key[1]]);
+
+            // arr = arr.map((item)=>{
+            //     console.log(item)
+                
+            //     return item == emoji[key[0]] ? emoji[key[1]] : item; // return문에는 if문을 못쓰기때문에  삼항연산자
+
+            // });  
+
+
+            // 식 단축
+            arr = arr.map(item => item == emoji[key[0]] ? emoji[key[1]] : item); // 이부분 굉장히 중요. return 생략. react에서 자주씀.
+
+
+            txt1.value = arr.join(' ');
+        })
+    }
+})
